574c5cd207cf1d8bf218e4016ae32a80ef4fb214
JornDallinga <jorndallinga@gmail.com>
2016-11-09 21:39:36 +0000

    added parameters, allows multiple folders as input

--- R/processProbaVbatch.R
+++ R/processProbaVbatch.R
@@ -6,6 +6,7 @@
 #' @param pattern Character. Only useful if x is of length 1. See \link{list.files} for more details
 #' @param tiles Character. Tiles to process, format "X10Y06",
 #' @param start_d Date. Starting date.
+#' @param end_d Date. End date.
 #' @param QC_val Numeric. See \code{\link{cleanProbaV}}
 #' @param fill Numeric. See \code{\link{cleanProbaV}}
 #' @param as.is Logical. See \code{\link{cleanProbaV}}
@@ -23,38 +24,69 @@
 #' @import doParallel
 #' @import foreach
 
-processProbaVbatch <- function(x, pattern = "NDVI.tif$", tiles=NULL, start_d=NULL, QC_val, fill=NULL, as.is=FALSE, outdir, ncores=1, overwrite=FALSE) {
+processProbaVbatch <- function(x, pattern = pattern, tiles=NULL, start_date=NULL, end_date=NULL, QC_val = QC_val, fill=NULL, as.is=FALSE, outdir, ncores=1, overwrite=FALSE) {
+
   if (!is.character(x)) {
     stop('x needs to be of class character')
   }
+  # Copy folder structure for name output
+  x2 <- x
 
   if(length(x) == 1) {
-    info <- getProbaVinfo(x, pattern=pattern)
 
-    x <- list.files(path=x, pattern=pattern, full.names=TRUE,  recursive = T, include.dirs = F, no.. = T)
+    info <- getProbaVinfo(x, pattern=pattern, tiles = tiles)
+
+    # x <- list.files(path=x, pattern=pattern, full.names=TRUE,  recursive = T, include.dirs = F, no.. = T)
+  } else {
+
+    df = list()
+    for (i in 1:length(x)){
+      dat <- getProbaVinfo(x[i], pattern = pattern, tiles = tiles)
+      df[[i]] <- dat # add it to your list
+    }
+    info <- do.call(rbind, df)
   }
   if (!is.null(tiles)) {
-    x <- x[info$tile %in% tiles]
+    info <- subset(info, info$tile %in% tiles)
+    # x <- x[info$tile %in% tiles]
   }
-  if (!is.null(start_d)) {
-    x <- x[info$date <= start_d]
+  if (!is.null(end_date) & !is.null(start_date)) {
+    info <- subset(info, info$date >= start_date & info$date <= end_date)
+
+  }
+    # When you only insert 1 directory
+  if(length(x) == 1) {
+    x <- info$date
+    # if you follow the YYYYMMDD structure with multiple folders
+  } else {
+    f <- gsub("-", "", info$date)
+    # subset YYYYMMDD for file names
+    gg <- subset(x2, str_sub(x2,-8,-1) %in% f)
+
+    # Create file names
+    x <- info
+    x <- paste0(gg,'/',x$fpath)
   }
 
   dir.create(outdir, showWarnings = FALSE, recursive = TRUE)
-  type <- dataType(raster(x[1]))
+
+  if (pattern == "NDVI.tif$"){
+    type <- "FLT4S"
+  } else {
+    type <- dataType(raster(x[1]))
+  }
 
   outnames <- file.path(outdir, gsub("\\.tif", "_sm.tif", basename(x)))
-    # for unstacking radiometry image, sm will be in band suffix, for checking we use the red band
   outnames <- gsub("RADIOMETRY_sm\\.tif", "RED0_sm.tif", outnames)
+
   if(!overwrite){
     x <- x[!file.exists(outnames)]
     outnames <- outnames[!file.exists(outnames)]
   }
+
   outnames <- gsub("_RED0_sm\\.tif", ".tif", outnames)
 
-
   cat("Processing", length(x), "files. Names: ", length(outnames), "\n")
-
 
   if (ncores > 1){
     registerDoParallel(ncores)
@@ -62,17 +94,32 @@ processProbaVbatch <- function(x, pattern = "NDVI.tif$", tiles=NULL, start_d=NUL
 
   xprocessed <- foreach(i=x, o=outnames, .combine = c, .multicombine = T, .inorder = F, .packages = c("raster", "rgdal"), .verbose = T ) %dopar% {
     cat("...out:", o)
-    r <- cleanProbaV(i, filename=o, QC_val = QC_val, fill=fill, datatype = type, as.is = as.is )
+    r <- cleanProbaV(i, filename=o, QC_val = QC_val, fill=fill, datatype = type, as.is = as.is, overwrite = overwrite )
     print(r)
     o
   }
 
   registerDoSEQ()
-  cat(length(xprocessed), " files processed")
+
+  if (length(xprocessed) == 0){
+    cat(length(xprocessed), " files processed, files allready exists or are out of range")
+  } else {
+    cat(length(xprocessed), " files processed")
+  }
+
+
+  # # delete if files exists
+  # f_exist <- subset(outnames, file.exists(outnames) == T)
+  # if (length(f_exist) > 0 & pattern != "NDVI.tif$"){
+  #   cat("\n","deleting temp files")
+  #   file.remove(f_exist)
+  #   cat(length(f_exist), " files removed")
+  # }
+
   return(xprocessed)
 
-# old...
-#     mcmapply(FUN=cleanProbaV, f_data=x, filename = outnames,
-#          MoreArgs = list(QC_val = QC_val, fill=fill, datatype = type, read_gdal=read_gdal), mc.cores=mc.cores)
+  # old...
+  #     mcmapply(FUN=cleanProbaV, f_data=x, filename = outnames,
+  #          MoreArgs = list(QC_val = QC_val, fill=fill, datatype = type, read_gdal=read_gdal), mc.cores=mc.cores)
 
 }
