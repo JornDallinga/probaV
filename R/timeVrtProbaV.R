33f4504d382439bfa469a6b4b1acf0d188dac4df
JornDallinga <jorndallinga@gmail.com>
2016-11-09 21:39:56 +0000

    fix

--- R/timeVrtProbaV.R
+++ R/timeVrtProbaV.R
@@ -7,15 +7,16 @@
 #'
 #' @author J Eberenz
 #' @param x Character. Directory of a Proba-V geotiffs or list of well formated filenames.
-#' @param patttern Character. As in \code{\link{list.files}}.
-#' @param stacked_bands Character. Which abnds are stacked (typically RADIOMETRY)
+#' @param pattern Character. As in \code{\link{list.files}}.
+#' @param stacked_bands Character. Which bands are stacked (typically RADIOMETRY)
 #' @param vrt_name Character. Output filename.
-#' @param orderChrono Logical. Wether to oder the stack chronologically.
+#' @param order Chrono Logical. Wether to order the stack chronologically.
 #' Default \code{TRUE}.
 #' @param tile Character. Which tile to process. Format: "X00Y00".
+#' @param start_date Date. First date to process.
 #' @param end_date Date. Last date to process.
-#' @param retunr_raster Logical. Weather a RasterBrick should eb returned.
-#' Else a info data.frame witht is returned (see \code{\link{getProbaVInfo}})
+#' @param return_raster Logical. Wether a RasterBrick should be returned.
+#' Else an info data.frame is returned (see \code{\link{getProbaVInfo}})
 #'
 #' @return a rasterBrick pointing to the vrt or a data.frame with meta data.
 #' @details
@@ -28,7 +29,7 @@
 #' @examples
 #'
 #'
-timeVrtProbaV <- function(x, pattern, stacked_bands=NULL, vrt_name, order_chrono=TRUE, tile=NULL, end_date=NULL, return_raster=TRUE){
+timeVrtProbaV <- function(x, pattern, stacked_bands=NULL, vrt_name, order_chrono=TRUE, tile=NULL, start_date = NULL, end_date=NULL, return_raster=TRUE, ...){
 
   df_info <- getProbaVinfo(x, pattern)
   if(order_chrono){
@@ -36,7 +37,15 @@ timeVrtProbaV <- function(x, pattern, stacked_bands=NULL, vrt_name, order_chrono
   }
 
   if (!is.null(tile)) df_info  <- df_info[df_info$tile==tile, ]
-  if (!is.null(end_date)) df_info  <- df_info[as.numeric(df_info$date) <= end_date,]
+  if (!is.null(end_date) & !is.null(start_date)){
+    df_info  <- df_info[(df_info$date) <= end_date & (df_info$date) >= start_date,]
+  } else if (is.null(end_date)) {
+    end_date <- max(df_info$date)
+  } else if (is.null(start_date)){
+    start_date <- min(df_info$date)
+  }
+
+  if (!is.null(end_date) & !is.null(start_date)) df_info  <- df_info[(df_info$date) <= end_date & (df_info$date) >= start_date,]
 
 
   if(!is.null(stacked_bands))  {
@@ -45,7 +54,7 @@ timeVrtProbaV <- function(x, pattern, stacked_bands=NULL, vrt_name, order_chrono
 
       for (b in 1:4){
         s_out <- gsub(s, pattern = "RADIOMETRY", replacement = c("RED0", "NIR0", "BLUE", "SWIR")[b])
-        gdalUtils::gdalbuildvrt(s, extension(s_out, "vrt"), b=b, overwrite = T, verbose=F)
+        gdalUtils::gdalbuildvrt(s, extension(s_out, "vrt"), b=b, overwrite = T, verbose=F, ...)
       }
 
     }
@@ -56,8 +65,10 @@ timeVrtProbaV <- function(x, pattern, stacked_bands=NULL, vrt_name, order_chrono
   }
 
   cat("\n building main vrt, layers:", nrow(df_info))
-  gdalUtils::gdalbuildvrt(df_info$fpath, vrt_name, separate = T, overwrite = T, verbose=F)
-
+  # new
+  gdalUtils::gdalbuildvrt(paste(x,df_info$fpath, sep = ""), vrt_name, separate = T, overwrite = T, verbose=F, ...)
+  # old (does not work)
+  # gdalUtils::gdalbuildvrt(df_info$fpath, vrt_name, separate = T, overwrite = T, verbose=F)
 
   if(return_raster) {
     b <- brick(vrt_name)
